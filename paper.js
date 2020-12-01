6ca9633fac8fd36c136e8ee22c5c759aa1398170
priyesh357 <73286031+priyesh357@users.noreply.github.com>
2020-12-01 17:05:23 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,23 @@
+class paper {
+    constructor(x,y,r) {
+      var options = {
+          isStatic: false,
+          restitution:0.3,
+          friction:0.5,
+          density:1.2
+      }
+      this.body = Bodies.circle(x,y,r,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      push();
+      translate(pos.x,pos.y)
+      ellipseMode(RADIUS);
+      fill("brown");
+      ellipse(0, 0, this.r, this.r);
+      pop();
+    }
+  };
